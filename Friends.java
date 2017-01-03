73d4be505d0670ad376f7cd597facf18c3d760d7
tmkrocks138 <tmkrocks138@gmail.com>
2017-01-03 15:37:28 -0500

    starting

--- Friends.java
+++ Friends.java
@@ -1,8 +1,11 @@
 //Abstract Friends
 public abstract class Friends {
     boolean cut;
+    boolean hangAfterSchool;
     int timeHome;
-    
+    boolean Facebook;
+    String FacebookMessage;
+   
     abstract void setCut();
     abstract void setTimeHome();
 }
