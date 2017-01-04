8207a550d9ed650e416b84bd6efbfbf48ed9e07a
tmkrocks138 <tmkrocks138@gmail.com>
2017-01-03 20:27:31 -0500

    Baseline

--- Friends.java
+++ Friends.java
@@ -8,5 +8,24 @@ public abstract class Friends {
    
     abstract void setCut();
     abstract void setTimeHome();
+ abstract void setHangAfterSchool();
+    abstract void setFacebook();
+    abstract void setFacebookMessage();
+
+    public boolean getCut(){
+	return cut;
+    }
+    public boolean getHangAfterSchool(){
+	return hangAfterSchool;
+    }
+    public int getTimeHome(){
+	return timeHome;
+    }
+    public boolean getFacebook(){
+	return Facebook;
+    }
+    public String getFacebookMessage(){
+	return FacebookMessage;
+    }
 }
 
