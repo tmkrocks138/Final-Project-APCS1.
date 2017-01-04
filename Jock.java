d3627eb610a0b056dc2936fa5e5070667e347e52
tmkrocks138 <tmkrocks138@gmail.com>
2017-01-03 23:05:23 -0500

    Jock start

---
+++ Jock.java
@@ -0,0 +1,22 @@
+public class Jock extends Friends{
+    private String sport;
+    
+    public void setCut(){
+	cut = false;
+	//possibly chance
+    }
+    public void setTimeHome(){
+	timeHome = 19;
+    }
+    public void setHangAfterSchool(){
+	hangAfterSchool = false;
+    }
+    public void setFacebook(){
+	Facebook = true;
+	//possibly chance
+    }
+    public void setFacebookMessage(){
+	FacebookMessage = "can you believe we have a test tomorrow?!? lol not gonna study";
+    }
+}
+
