941a552c39c05e0276200a7eff4a3c87daa088b0
Evan Chen <echen10@stuy.edu>
2017-01-04 22:28:07 -0500

    Testing out nested layouts

--- Character.java
+++ Character.java
@@ -3,24 +3,31 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Character extends JFrame /*implements ActionListener*/ {
-    private Container pane;
-    private JLabel username;
-    private JButton b;
+    private Container paneBox, paneTitle, paneInputs;
+    private JLabel title, inputName;
     private JTextField name;
+    private JButton submit;
 
     public Character() {
+	//paneBox
 	this.setTitle("Make your Stuy Student");
 	this.setSize(500,500);
 	this.setLocation(100,100);
 	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-	username = new JLabel("Name:", null, JLabel.CENTER);
+	//title = new JLabel("Character Info");
+	//title.setFont( new Font("Serif", Font.PLAIN, 18) );
+        inputName = new JLabel("Name:", null, JLabel.CENTER);
 	name = new JTextField(16);
 
-	pane = this.getContentPane();
-	pane.setLayout( new BoxLayout(pane, BoxLayout.Y_AXIS) );
-	pane.add(name);
-	pane.add(username);
+	//paneTitle = this.getContentPane();
+	//System.out.println(pane);
+	//paneTitle.setLayout( new BoxLayout(paneTitle, BoxLayout.Y_AXIS) );
+
+	paneInputs = this.getContentPane();
+	//paneInputs.add(title);
+	//paneInputs.add(inputName);
+	//paneInputs.add(name);
 
     }
     
