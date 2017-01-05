e5a67ae425b9a1836fde21d4e7af80ccc5c7ac61
tmkrocks138 <tmkrocks138@gmail.com>
2017-01-05 15:13:49 -0500

    this better work...

---
+++ STC.java
@@ -0,0 +1,23 @@
+public class STC extend Friends{
+    private String play = "";
+    
+    public void setCut(){
+	cut = false;
+	//possibly chance
+    }
+    public void setTimeHome(){
+	timeHome = 20;
+    }
+    public void setHangAfterSchool(){
+	hangAfterSchool = false;
+    }
+    public void setFacebook(){
+	Facebook = true;
+	//possibly chance
+    }
+    public void setFacebookMessage(){
+	FacebookMessage = "dont forget to buy txt"+play;
+    }
+}
+
+
