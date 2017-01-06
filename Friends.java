83cab1f685bc0813dbbb0f59e9883d11ca14fd57
tmkrocks138 <tmkrocks138@gmail.com>
2017-01-06 17:52:29 -0500

    AfterSchool and Cut

--- Friends.java
+++ Friends.java
@@ -1,16 +1,10 @@
-//Abstract Friends
-public abstract class Friends {
+public abstract class Friends{
     boolean cut;
     boolean hangAfterSchool;
-    int timeHome;
-    boolean Facebook;
-    String FacebookMessage;
-   
+    
     abstract void setCut();
     abstract void setTimeHome();
- abstract void setHangAfterSchool();
-    abstract void setFacebook();
-    abstract void setFacebookMessage();
+
 
     public boolean getCut(){
 	return cut;
@@ -18,14 +12,6 @@ public abstract class Friends {
     public boolean getHangAfterSchool(){
 	return hangAfterSchool;
     }
-    public int getTimeHome(){
-	return timeHome;
-    }
-    public boolean getFacebook(){
-	return Facebook;
-    }
-    public String getFacebookMessage(){
-	return FacebookMessage;
-    }
+
 }
 
