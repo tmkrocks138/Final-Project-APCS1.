7c0dc1f614c1d8406df0c22bbb2b8cdcec79f32d
echen10 <echen10@stuy.edu>
2017-01-06 15:25:14 -0500

    Named traits

--- Character.java
+++ Character.java
@@ -13,24 +13,24 @@ public class Character extends JFrame /*implements ActionListener*/ {
     @SuppressWarnings("unchecked")
     public Character() {
 	this.setTitle("Make your Stuy Student");
-	this.setSize(500,300);
+	this.setSize(400,300);
 	this.setLocation(100,100);
 	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 	title = new JLabel("Character Info");
 	title.setFont( new Font("Serif", Font.PLAIN, 18) );
         inputName = new JLabel("Name:", null, JLabel.CENTER);
-	name = new JTextField(16);
+	name = new JTextField(8);
 	inputSubjects = new JLabel("Choose your favorite class:", null, JLabel.CENTER);
 	String[] subjectList = {"Science", "Math", "History", "Computer Science"};
 	subjects = new JComboBox(subjectList);
 	submit = new JButton("Submit");
 	inputTraits = new JLabel("Choose your traits:", null, JLabel.CENTER);
-	trait1 = new JCheckBox("Trait1");
-	trait2 = new JCheckBox("Trait2");
-	trait3 = new JCheckBox("Trait3");
-	trait4 = new JCheckBox("Trait4");
-	trait5 = new JCheckBox("Trait5");
+	trait1 = new JCheckBox("Hardworking");
+	trait2 = new JCheckBox("Shy");
+	trait3 = new JCheckBox("Lazy");
+	trait4 = new JCheckBox("Friendly");
+	trait5 = new JCheckBox("Forgetful");
 	
 	
 	pane = this.getContentPane();
