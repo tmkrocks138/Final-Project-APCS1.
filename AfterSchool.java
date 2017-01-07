78457c50ab79f7b88fa420ad0689c7927a682ada
tmkrocks138 <tmkrocks138@gmail.com>
2017-01-07 17:01:10 -0500

    Finished...for now

--- AfterSchool.java
+++ AfterSchool.java
@@ -2,9 +2,8 @@ import java.lang.Math;
 
 public class AfterSchool{
 
-    public boolean busy;
-
     public boolean isBusy(String trait){
+	boolean busy = true;
 	int num = (int)(Math.random() * 10)+1;
 	if (trait == "Shy"){
 	    if (num % 3 == 0){
@@ -24,6 +23,28 @@ public class AfterSchool{
 	return busy;
     }
 
+ public void scene(String trait, boolean bff){
+	boolean bee = this.isBusy(trait);
+	System.out.println("After a long hard day of classes, you have finally reached the end of the day!!");
+	System.out.println("Today, you have no activities to do after school");
+	if(bee = true){
+	    System.out.println("Because of that, you have chosen to go home on time");
+	    System.out.println("You need to get a start on your homework");
+	}
+	
+	else if (bff == false){
+	System.out.println("You want to hang out with a friend since you have tie");
+	System.out.println("However, your best friend is busy so you have to go home");
+	}
+	else{
+	    System.out.println("Luckily, your best friend had nothing to do after school as well");
+	    System.out.println("The two of you decided to hang out but you got to school");
+	}
+
+    }
+
+
 }
+
 
 
