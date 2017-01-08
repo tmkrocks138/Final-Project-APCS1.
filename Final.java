5cd2f47aa38cb7924c0285de3116b7b0ecc4ec20
tmkrocks138 <tmkrocks138@gmail.com>
2017-01-07 19:58:43 -0500

    Outline

--- Final.java
+++ Final.java
@@ -1,1 +1,39 @@
+public class Final{
+/*
+This is all of the parts added together
+*/
 
+//Create a character
+
+
+
+
+//Wake up
+
+
+
+
+
+//Class
+
+
+
+
+
+//After School
+
+
+
+
+
+
+//Homework
+
+
+
+
+
+
+
+
+}
