e5da5673312ba73a33e574eb1749a0e788b33ada
tmkrocks138 <tmkrocks138@gmail.com>
2017-01-07 20:26:16 -0500

    Frame

--- Final.java
+++ Final.java
@@ -3,6 +3,11 @@ public class Final{
 This is all of the parts added together
 */
 
+    public static void main(String[] args){
+	int social = 0;
+	int school = 0;
+	int stress = 0;
+
 //Create a character
 
 
@@ -34,6 +39,6 @@ This is all of the parts added together
 
 
 
-
+}
 
 }
