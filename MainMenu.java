037a48383a90ccbafa61fd0f199dbed0504fe334
Evan Chen <echen10@stuy.edu>
2017-01-08 13:56:01 -0500

    Added a main menu

---
+++ MainMenu.java
@@ -0,0 +1,50 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class MainMenu extends JFrame implements ActionListener {
+    private Container pane;
+    private JLabel title;
+    private JButton newCharacter, newGame, continueGame, options, quit;
+
+    public MainMenu() {
+	this.setTitle("Main Menu");
+	this.setSize(400,200);
+	this.setLocation(100,100);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+	//title = new JLabel ("Life of a Stuy Student");
+	newCharacter = new JButton("Make a New Character");
+	newGame = new JButton("Start a New Game");
+	continueGame = new JButton("Continue Game");
+	options = new JButton("Options");
+	quit = new JButton("Quit");
+	
+	pane = this.getContentPane();
+	//pane.setLayout( new BoxLayout(pane, BoxLayout.Y_AXIS) );
+	pane.setLayout( new BorderLayout() );
+	
+	quit.addActionListener(this);
+	quit.setActionCommand("quit");
+	
+	//pane.add(title, BorderLayout.PAGE_START);
+	pane.add(newCharacter, BorderLayout.PAGE_START);
+	pane.add(newGame, BorderLayout.LINE_START);
+	pane.add(continueGame, BorderLayout.CENTER);
+	pane.add(options, BorderLayout.LINE_END);
+	pane.add(quit, BorderLayout.PAGE_END);
+    }
+
+
+    public void actionPerformed(ActionEvent e) {
+	String event = e.getActionCommand();
+	if ( event.equals("quit") ) {
+	    System.exit(0);
+	}
+    }
+    
+    public static void main(String[] args) {
+	MainMenu mm = new MainMenu();
+	mm.setVisible(true);
+    }
+}
