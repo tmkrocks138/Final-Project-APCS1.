298c81849fb62523888c801aa11450c27309e479
tmkrocks138 <tmkrocks138@gmail.com>
2017-01-08 20:14:54 -0500

    Started

---
+++ Homework.java
@@ -0,0 +1,13 @@
+public class Homework{
+    private int option;
+    //1-->Stay up late to finish
+    //2-->Sacrafice CS and sleep early
+    //3-->Sacrafice History and sleep early
+    //4-->Mental Health day tomorrow
+
+
+    public int getOption(){
+	return option;
+    }
+
+}
