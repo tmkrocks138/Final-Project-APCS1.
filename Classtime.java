7c84ffc5bbe7aa2dd33bcd76c0b42e01ba9c81d5
tmkrocks138 <tkhanna@stuy.edu>
2017-01-09 11:19:54 -0500

    Cut option done

--- Classtime.java
+++ Classtime.java
@@ -2,9 +2,14 @@ import java.util.Scanner;
 public class Classtime{
 
     public boolean cut;
+    public String where;
 
     public boolean getCut(){
 	return cut;
+    }
+
+    public String getWhere(){
+	return where;
     }
     
     public void scene(String trait, boolean friendCut){
@@ -26,7 +31,29 @@ public class Classtime{
     }
 
     public void cutScene (boolean friendCut){
+	Scanner user_input = new Scanner( System.in );
+	if(friendCut){
+	    System.out.println("Your best friend also decided to cut this with you");
+	}
+	else{
+	    System.out.println("Sadly, your best friend does not want to cut with you");
+	}
+	System.out.println("You can either go to the half floor to hang out");
+	System.out.println("or go the cafeteria to study.");
+	System.out.println("Type 1 to go to the half floor");
+	System.out.println("Type 2 to go to the cafeteria");
+	System.out.println("");
+        where = user_input.next();
+	System.out.println("");
 
+	if (where.equals("1")){
+	    System.out.println("You relax on the half floor but go to your later classes");
+	    System.out.println("");
+	}
+	else if (where.equals("2")){
+	    System.out.println("You start on some of your homework but go to your later classes");
+	}
+	
     }
     
     public void noCutScene(){
