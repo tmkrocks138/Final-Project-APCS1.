7a9944ae07a15fbd88dd310bed933a06ff258be0
Evan Chen <echen10@stuy.edu>
2017-01-10 22:08:55 -0500

    Worked on alarmClock and a timer for wakUpFriend

--- WakeUpFriend.java
+++ WakeUpFriend.java
@@ -2,15 +2,16 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class WakeUpFriend extends JFrame implements ActionListener {
+public class WakeUpFriend extends JFrame {
     private Container pane;
     private JLabel info, numPokes, time;
+    private Timer timer;
     private JButton poke;
     private int counter, timeLeft;
 
     public WakeUpFriend() {
 	this.setTitle("Wake up your friend!");
-	this.setSize(300,300);
+	this.setSize(300,400);
 	this.setLocation(100,100);
 	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
@@ -19,14 +20,18 @@ public class WakeUpFriend extends JFrame implements ActionListener {
 	
 	info = new JLabel("Poke your friend a few times to wake him up.");
 	time = new JLabel(timeLeft + "");
+	timer = new Timer(1000, this);
 	numPokes = new JLabel(counter + "");
 	poke = new JButton( new ImageIcon("Sleeping.png") );
 
 	pane = this.getContentPane();
 	pane.setLayout( new BoxLayout(pane, BoxLayout.PAGE_AXIS) );
+
+	
 	
 	poke.addActionListener(this);
 	poke.setActionCommand("Poked");
+	timer.setActionCommand("Ticked");
 
 	pane.add(info);
 	pane.add(time);
@@ -39,11 +44,19 @@ public class WakeUpFriend extends JFrame implements ActionListener {
 	String event = e.getActionCommand();
 	if ( event.equals("Poked") ) {
 	    counter += 1;
-	}	
+	    numPokes.setText(counter+"");
+	    //System.out.println(counter);
+	}
+	if ( event.equals("Ticked") ) {
+	    time.setText(timeLeft+"");
+	}
+	    
     }
     
     public static void main(String[] args) {
 	WakeUpFriend a = new WakeUpFriend();
+	Timer t = new Timer();
 	a.setVisible(true);
+	t.start();
     }
 }
