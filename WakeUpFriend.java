89ce063993e776f2422fc293bf37f8cb1265fcf4
Evan Chen <echen10@stuy.edu>
2017-01-10 22:39:41 -0500

    Timers are difficult :C

--- WakeUpFriend.java
+++ WakeUpFriend.java
@@ -3,18 +3,14 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class WakeUpFriend extends JFrame {
-    private Container pane;
     private JLabel info, numPokes, time;
     private Timer timer;
     private JButton poke;
     private int counter, timeLeft;
 
     public WakeUpFriend() {
-	this.setTitle("Wake up your friend!");
-	this.setSize(300,400);
-	this.setLocation(100,100);
-	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-
+        setLayout( new BoxLayout(pane, BoxLayout.PAGE_AXIS) );
+	
 	counter = 0;
 	timeLeft = 10;
 	
@@ -23,23 +19,52 @@ public class WakeUpFriend extends JFrame {
 	timer = new Timer(1000, this);
 	numPokes = new JLabel(counter + "");
 	poke = new JButton( new ImageIcon("Sleeping.png") );
-
-	pane = this.getContentPane();
-	pane.setLayout( new BoxLayout(pane, BoxLayout.PAGE_AXIS) );
-
 	
-	
+        add(info);
+        add(time);
+        add(numPokes);
+        add(poke);
+
+	event e = new event();
+	poke.addActionListener(e);
+
+    }
+
+    public class event implements ActionListener {
+	public void actionPerformed(ActionEvent e) {
+	    timerLabel.setText(timeLeft + "");
+
+	    TimeClass tc = new TimeClass(counter);
+	    timer = new Timer(1000, tc);
+	    timer.start();
+	}
+    }
+		       
 	poke.addActionListener(this);
 	poke.setActionCommand("Poked");
 	timer.setActionCommand("Ticked");
 
-	pane.add(info);
-	pane.add(time);
-	pane.add(numPokes);
-	pane.add(poke);
+    public class TimeClass implements ActionListener {
+	int counter;
 
+	public TimeCLass(int counter) {
+	    this.counter = counter;
+	}
+
+	public void actionPerformed(ActionEvent tc) {
+	    counter--;
+
+	    if ( counter >= 1) {
+		timerLabel.setText("Time left: " + counter);
+	    }
+	    else {
+		time.stop();
+		timeLabel.setText("Out of time!");
+	    }
+	}
     }
-
+    
+    /*
     public void actionPerformed(ActionEvent e) {
 	String event = e.getActionCommand();
 	if ( event.equals("Poked") ) {
@@ -52,10 +77,17 @@ public class WakeUpFriend extends JFrame {
 	}
 	    
     }
+    */
     
     public static void main(String[] args) {
 	WakeUpFriend a = new WakeUpFriend();
 	Timer t = new Timer();
+
+	this.setTitle("Wake up your friend!");
+	this.setSize(300,400);
+	this.setLocation(100,100);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	
 	a.setVisible(true);
 	t.start();
     }
