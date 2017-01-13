1c5d669509307c9464d5d12a0fc8055876f7a790
tmkrocks138 <tkhanna@stuy.edu>
2017-01-13 15:31:11 -0500

    fixing git stuff

--- Classtime.java
+++ Classtime.java
@@ -1,8 +1,17 @@
 import java.util.Scanner;
+import java.lang.Math;
+
 public class Classtime{
 
     public boolean cut;
     public String where;
+    public boolean sleep;    
+    public String wakeFriend;
+    
+
+    public boolean getSleep(){
+	return sleep;
+    }
 
     public boolean getCut(){
 	return cut;
@@ -11,8 +20,11 @@ public class Classtime{
     public String getWhere(){
 	return where;
     }
+public String getWakeFriend(){
+	return wakeFriend;
+}
     
-    public void scene(String trait, boolean friendCut){
+    public void scene(String trait, boolean friendCut, String subject, boolean late){
 	Scanner user_input = new Scanner( System.in );
 	System.out.println("After finishing some of your classes, you hear a rumor you have a sub next period");
 	System.out.println("Do you want to cut that class? (Type yes or no)");
@@ -21,9 +33,9 @@ public class Classtime{
 	    cut = true;
 	    this.cutScene(friendCut);
 	}
-	else if(cutS == "no"){
+	else{
 	    cut = false;
-	    this.noCutScene();
+	    this.noCutScene(trait, subject, late);
 	}
 	
 	System.out.println("");
@@ -56,9 +68,43 @@ public class Classtime{
 	
     }
     
-    public void noCutScene(){
+    public void noCutScene(String trait, String subject, boolean lateToSchool){
+	int num = (int)(Math.random() * 10)+1;
+        Scanner user_input = new Scanner( System.in );	
+        System.out.println("");
+        System.out.println(subject);
+	System.out.println("It seems to be a good idea to go to class because your normal teacher was there");
+	System.out.println("The "+ subject +" class you were in was particularly boring");
+	System.out.println("");
+	
+	if(trait == "Lazy" && !lateToSchool){
+	    sleep = true;
+	    System.out.println("Since you came into school on time and you are lazy, you  slowly dosed off");
+	    
+	}
+	else if (!lateToSchool && (num % 3 == 0)){
+	    sleep = true;
+	    System.out.println("Since you came into school ontime, you were tired and slowly dozed off");
+	}
+	else{
+	    sleep = false;
+	    System.out.println("You somehow manage to stay awake, even if you aren't listening");
+	    System.out.println("You notice that one of your friends is sleeping next to you");
+	    System.out.println("Do you want to wake them up or not? (Type yes or no)");
+	    System.out.println("");
 
+	    wakeFriend = user_input.next();
+
+	    if(wakeFriend.equals("no")){
+		System.out.println("");
+		System.out.println("Sadly, your friend gets yelled at. However it is better them than you");
+	    }
+	    else{
+		System.out.println("");
+		System.out.println("Yay, you looked out for your friend");
+		}
+
+	}
     }
 
 }
-
