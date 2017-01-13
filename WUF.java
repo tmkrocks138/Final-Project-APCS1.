207e72c4e08ad27e81379788068da56fa40575ea
Evan Chen <echen10@stuy.edu>
2017-01-12 23:49:17 -0500

    Got the timer to work

--- WUF.java
+++ WUF.java
@@ -5,53 +5,66 @@ import java.awt.event.*;
 public class WUF extends JFrame implements ActionListener {
     private Container pane;
     private JLabel info, numPokes, time;
-    private JButton poke;
+    private JButton poke, submit;
     private Timer timer;
     private int counter, timeLeft;
 
     public WUF() {
 	this.setTitle("Wake up your friend!");
-	this.setSize(300,300);
+	this.setSize(300,400);
 	this.setLocation(100,100);
 	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+	//COMPONENTS
 	counter = 0;
 	timeLeft = 10;
-	
+
 	info = new JLabel("Poke your friend a few times to wake him up.");
 	time = new JLabel("Time Left: " + timeLeft);
-	numPokes = new JLabel(counter + "");
+	numPokes = new JLabel("Clickies: " + counter);
 	poke = new JButton( new ImageIcon("Sleeping.png") );
-	timer = new Timer(1000, this);
+	submit = new JButton("Done!");
 
+	//TIMER
+	timer = new Timer(1000, this);
+	timer.start();
+	
+	//LAYOUT
 	pane = this.getContentPane();
 	pane.setLayout( new BoxLayout(pane, BoxLayout.PAGE_AXIS) );
-	
+
+	//ACTION LISTENER STUFF
 	poke.addActionListener(this);
 	poke.setActionCommand("Poked");
-	timer.setActionListener("Ticked");
+	timer.setActionCommand("Ticked");
 
+	//ADD COMPONENTS ONTO GUI
 	pane.add(info);
 	pane.add(time);
 	pane.add(numPokes);
 	pane.add(poke);
-
-	timer.start();
+	pane.add(submit);
 
     }
 
     public void actionPerformed(ActionEvent e) {
 	String event = e.getActionCommand();
-	if ( event.equals("Poked") ) {
+	if ( event.equals("Poked") && timeLeft != 0) {
 	    counter += 1;
+	    numPokes.setText("Clickies: " + counter);
 	}
 	if ( event.equals("Ticked") ) {
 	    timeLeft--;
-	    time.setText(timeLeft+"");
-	    if ( timeLeft == 1 ) {
+	    time.setText("Time Left: " + timeLeft);
+	    if ( timeLeft == 0 ) {
 		timer.stop();
+		time.setText("Times up!");
 	    }
 	}
+    }
+
+    public String getClicks() {
+	return counter+"";
     }
     
     public static void main(String[] args) {
