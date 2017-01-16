4dded212c62adc8834a858eff6ff2e471ba7c6c3
Evan Chen <echen10@stuy.edu>
2017-01-15 21:51:58 -0500

    Need to add some files for questions and answers

--- Quiz.java
+++ Quiz.java
@@ -5,27 +5,52 @@ import java.awt.event.*;
 public class Quiz extends JFrame {
     private Container pane;
     private JLabel title;
+    private String subject;
+    private String[] questions, answers;
     private JButton submit;
 
     public Quiz() {
 	this.setTitle("Quiz Time!");
 	this.setSize(500,500);
-	//this.setLocation(100,100);
+	this.setLocation(500,100);
 	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 	//COMPONENTS
-	title = new JLabel("[title to be replaced]");
+	title = new JLabel(subject+" Quiz" );
+
+	
+	subject = Character.getSubject();
+        if ( subject.equals("Science") ) {
+	    questions = 
+	}
+	else if ( subject.equals("Math") ) {
+	    
+	}
+	else if ( subject.equals("History") ) {
+	    
+	}
+	else if ( subject.equals("Computer Science") ) {
+	    
+	}
+	else {
+	    
+	}
+	    
+	
+	
 	submit = new JButton("Done");
 	
 	//LAYOUT
 	pane = this.getContentPane();
-	//pane.setLayout();
+	pane.setLayout( new BoxLayout(pane, BoxLayout.Y_AXIS) );
 
 	//ACTION LISTENER STUFF
 
 	//ADD COMPONENTS ONTO GUI
 	pane.add(title);
-	//pane.add(submit);
+	
+	
+	pane.add(submit);
     }
 
     public static void main(String[] args) {
